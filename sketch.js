4bf76dedaa07f3fc19bf591acab1ef425fe89cca
Param-2006 <69540098+Param-2006@users.noreply.github.com>
2020-09-15 14:05:18 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var bullet,wall;
+var speed,weight;
+var thickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+   thickness=random(22,83);
+
+
+  bullet=createSprite(50,200,50,20);
+   wall=createSprite(1200,200,thickness,height/2);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet.velocityX = speed;
+
+}
+
+function draw() 
+{
+  background(255,255,255); 
+  
+  
+
+    if(hasCollided(wall,bullet))
+    {
+      bullet.velocityX=0;
+      var damage=0.5 *weight *speed* speed/(thickness *thickness *thickness);
+  
+  
+      if(damage>10)
+      {
+        wall.shapeColor=color(255,0,0);
+      }
+  
+  
+      if(damage<10)
+      {
+        wall.shapeColor=color(0,225,0);
+      }
+  
+    }
+     
+    
+
+    
+
+  drawSprites();
+
+  }
+
+
+
+  function hasCollided(lbullet,lwall)
+  {
+    bulletRightEdge=lbullet.x +lbullet.width/2;
+    wallLeftEdge=lwall.x-lwall.width/2;
+    if(bulletRightEdge>=wallLeftEdge)
+    {
+      return true
+    }
+    return false;
+  }
+
+ 
+
+
+    
+
+  
