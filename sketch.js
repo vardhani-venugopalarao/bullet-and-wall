2669c93849d906f8dca7163e71ccdad6b5e5fd86
vardhani-venugopalarao <68939700+vardhani-venugopalarao@users.noreply.github.com>
2020-09-17 01:05:22 +0530

    Update sketch.js
    Modified to get output

--- sketch.js
+++ sketch.js
@@ -1,71 +1,61 @@
-var bullet,wall;
-var speed,weight;
-var thickness;
-
-
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
-   thickness=random(22,83);
+  createCanvas(1600, 400);
 
-
-  bullet=createSprite(50,200,50,20);
-   wall=createSprite(1200,200,thickness,height/2);
   speed=random(223,321);
   weight=random(30,52);
-  bullet.velocityX = speed;
+  thickness=random(22,83);
 
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor="white";
+ 
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor="white";
+  
 }
 
-function draw() 
-{
-  background(255,255,255); 
-  
-  
 
-    if(hasCollided(wall,bullet))
-    {
-      bullet.velocityX=0;
-      var damage=0.5 *weight *speed* speed/(thickness *thickness *thickness);
-  
-  
-      if(damage>10)
-      {
-        wall.shapeColor=color(255,0,0);
-      }
-  
-  
-      if(damage<10)
-      {
-        wall.shapeColor=color(0,225,0);
-      }
-  
-    }
-     
-    
+function draw() {
+  background(0);  
 
-    
+  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+  if(hasCollided(bullet, wall))
+  {
+    bullet.velocityX=0;
+    bullet.visible = false;
+	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
+
 
   drawSprites();
-
-  }
-
-
-
-  function hasCollided(lbullet,lwall)
-  {
-    bulletRightEdge=lbullet.x +lbullet.width/2;
-    wallLeftEdge=lwall.x-lwall.width/2;
-    if(bulletRightEdge>=wallLeftEdge)
-    {
-      return true
-    }
-    return false;
-  }
-
  
+}
 
 
-    
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width/2;
+  wallLeftEdge=lwall.x -lwall.width/2;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true;
+	}
+	return false;
+}
 
   
